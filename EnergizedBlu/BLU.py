845af084324d753c709b4dd0051a6e0794cbe10b
Ador <adroit.ador@live.com>
2018-05-23 15:46:54 +0600

    Updated.

--- EnergizedBlu/BLU.py
+++ EnergizedBlu/BLU.py
@@ -16,7 +16,7 @@ List = []
 Sources = [
 	'https://raw.githubusercontent.com/AdroitAdorKhan/Energized/master/EnergizedHosts/EnergizedHosts',
 	'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts',
-	'http://someonewhocares.org/hosts/zero/hosts',
+	'https://raw.githubusercontent.com/EnergizedProtection/EnergizedHosts/master/EnergizedAd/data/someonewhocares.org/hosts',
 	'https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt',
 	'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts',
 	'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts',
