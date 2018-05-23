6092df6035e323edb1b1f584841dbd8477efe33d
Ador <adroit.ador@live.com>
2018-05-23 16:08:27 +0600

    Test.

--- EnergizedBlu/BLU.py
+++ EnergizedBlu/BLU.py
@@ -17,6 +17,7 @@ Sources = [
 	'https://raw.githubusercontent.com/AdroitAdorKhan/Energized/master/EnergizedHosts/EnergizedHosts',
 	'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts',
 	'https://raw.githubusercontent.com/EnergizedProtection/EnergizedHosts/master/EnergizedAd/data/someonewhocares.org/hosts',
+	#'https://hblock.molinero.xyz/hosts',
 	'https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt',
 	'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts',
 	'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts',
@@ -31,8 +32,7 @@ Sources = [
 	'https://raw.githubusercontent.com/EnergizedProtection/EnergizedTools/master/Converter/Hosts/EasyPrivacySpecific.txt',
 	'https://raw.githubusercontent.com/EnergizedProtection/EnergizedTools/master/Converter/Hosts/DisconnectMEAds.txt',
 	'https://raw.githubusercontent.com/EnergizedProtection/EnergizedTools/master/Converter/Hosts/DisconnectMEMalvertising.txt',
-    'https://raw.githubusercontent.com/EnergizedProtection/EnergizedTools/master/Converter/Hosts/DisconnectMEMalware.txt',
-	'https://hblock.molinero.xyz/hosts'
+    'https://raw.githubusercontent.com/EnergizedProtection/EnergizedTools/master/Converter/Hosts/DisconnectMEMalware.txt'
 ]
 
 for Link in Sources:
