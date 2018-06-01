11a025d396ea94df5aa077ead69183f50ce9d8a6
AdroitAdorKhan <adroit.ador@live.com>
2018-06-01 22:55:25 +0000

    Updated.
    - Added Zip Formats. 
    - Several Fixes.

--- EnergizedBlu/BLU.sh
+++ EnergizedBlu/BLU.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-# Energized Blu - Android Flashable ZIP Creator
+# Energized - ZIP Creator
+# Energized Blu Android Flashable & Windows Bat Installer Creator
 #
 HOSTS_URL=https://raw.githubusercontent.com/EnergizedProtection/EnergizedBlu/master/EnergizedBlu/energized/blu
+ 
+wget -O zip/Android/system/etc/hosts $HOSTS_URL
+wget -O zip/Windows/hosts $HOSTS_URL
+wget -O zip/Compressed/hosts $HOSTS_URL
 
-wget -O zip/system/etc/hosts $HOSTS_URL
-
-cd zip
-zip -9 -r ../energized/blu.zip .
+cd zip/Android
+zip -9 -r ../../energized/blu_android.zip .
+cd ..
+cd ../zip/Windows
+zip -9 -r ../../energized/blu_windows.zip .
+cd ..
+cd ../zip/Compressed
+zip -9 -r ../../energized/blu.zip .
 cd ..
