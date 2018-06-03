37e5b71a8f72f5b0ce14fef1c97b9a58fc168a3e
adroitadorkhan <adroitadorkhan@protonmail.com>
2018-06-03 18:00:04 -0400

    Updated.

--- EnergizedBlu/BLU.sh
+++ EnergizedBlu/BLU.sh
@@ -5,11 +5,15 @@
 HOSTS_URL=https://raw.githubusercontent.com/EnergizedProtection/EnergizedBlu/master/EnergizedBlu/energized/blu
  
 wget -O zip/Android/system/etc/hosts $HOSTS_URL
+wget -O zip/Magisk/system/etc/hosts $HOSTS_URL
 wget -O zip/Windows/hosts $HOSTS_URL
 wget -O zip/Compressed/hosts $HOSTS_URL
 
 cd zip/Android
 zip -9 -r ../../energized/blu_android.zip .
+cd ..
+cd ../zip/Magisk
+zip -9 -r ../../energized/blu_magisk.zip .
 cd ..
 cd ../zip/Windows
 zip -9 -r ../../energized/blu_windows.zip .
