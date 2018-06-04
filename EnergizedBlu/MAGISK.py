09211bea4fcf725986ed911956bdd75004c8b495
adroitadorkhan <adroitadorkhan@protonmail.com>
2018-06-04 02:19:35 -0400

    Updated.

--- EnergizedBlu/MAGISK.py
+++ EnergizedBlu/MAGISK.py
@@ -14,6 +14,7 @@ if not os.path.exists(os.path.dirname(File)):
 	os.makedirs(os.path.dirname(File))
 
 with open(File, 'w') as f:
+	print('[+ Magisk Module.Prop Updater +]')
 	print('[+] Writing to file...')
 	f.write('''id=energizedblu''')
 	f.write('''\nname=Energized Blu''')
