de1472627c74819f8a167c28320f4ddf6871aa83
adroitadorkhan <adroitadorkhan@protonmail.com>
2018-06-05 10:56:59 -0400

    Introducing Energized Blu go.

--- EnergizedBlu/BLU.sh
+++ EnergizedBlu/BLU.sh
@@ -2,12 +2,16 @@
 # Energized - ZIP Creator
 # Energized Blu Android Flashable & Windows Bat Installer Creator
 #
-HOSTS_URL=https://raw.githubusercontent.com/EnergizedProtection/EnergizedBlu/master/EnergizedBlu/energized/blu
+BLU=https://raw.githubusercontent.com/EnergizedProtection/EnergizedBlu/master/EnergizedBlu/energized/blu
+BLU_GO=https://raw.githubusercontent.com/EnergizedProtection/EnergizedBlu/master/EnergizedBlu/energized/blu_go
  
-wget -O zip/Android/system/etc/hosts $HOSTS_URL
-wget -O zip/Magisk/system/etc/hosts $HOSTS_URL
-wget -O zip/Windows/hosts $HOSTS_URL
-wget -O zip/Compressed/hosts $HOSTS_URL
+wget -O zip/Android/system/etc/hosts $BLU
+wget -O zip_go/Android/system/etc/hosts $BLU_GO
+wget -O zip/Magisk/system/etc/hosts $BLU
+wget -O zip_go/Magisk/system/etc/hosts $BLU_GO
+wget -O zip/Windows/hosts $BLU
+wget -O zip/Compressed/hosts $BLU
+wget -O zip_go/Compressed/hosts $BLU_GO
 
 cd zip/Android
 zip -9 -r ../../energized/blu_android.zip .
@@ -20,4 +24,13 @@ zip -9 -r ../../energized/blu_windows.zip .
 cd ..
 cd ../zip/Compressed
 zip -9 -r ../../energized/blu.zip .
+cd ..
+cd ../zip_go/Android
+zip -9 -r ../../energized/blu_go_android.zip .
+cd ..
+cd ../zip_go/Magisk
+zip -9 -r ../../energized/blu_go_magisk.zip .
+cd ..
+cd ../zip_go/Compressed
+zip -9 -r ../../energized/blu_go.zip .
 cd ..
--- EnergizedBlu/MAGISK.py
+++ EnergizedBlu/MAGISK.py
@@ -8,12 +8,13 @@ import datetime
 import os
 import time
 
-File = 'zip/Magisk/module.prop'
+Blu = 'zip/Magisk/module.prop'
+Blu_go = 'zip_go/Magisk/module.prop'
 
-if not os.path.exists(os.path.dirname(File)):
-	os.makedirs(os.path.dirname(File))
+if not os.path.exists(os.path.dirname(Blu)):
+	os.makedirs(os.path.dirname(Blu))
 
-with open(File, 'w') as f:
+with open(Blu, 'w') as f:
 	print('[+] Magisk Module.Prop Updater')
 	print('[+] Writing to file...')
 	f.write('''id=energizedblu''')
@@ -22,5 +23,20 @@ with open(File, 'w') as f:
 	f.write('''\nversionCode=''' + time.strftime("%j", time.gmtime()) + '''''')
 	f.write('''\nauthor=AdroitAdorKhan''')
 	f.write('''\ndescription=Lightweight Energized Protection.''')
+	f.write('''\nminMagisk=1500''')
+	print('[+] Done!')
+
+if not os.path.exists(os.path.dirname(Blu_go)):
+	os.makedirs(os.path.dirname(Blu_go))
+
+with open(Blu_go, 'w') as f:
+	print('[+] Magisk Module.Prop Updater')
+	print('[+] Writing to file...')
+	f.write('''id=energizedblugo''')
+	f.write('''\nname=Energized Blu go''')
+	f.write('''\nversion=1.0.''' + time.strftime("%m.%j", time.gmtime()) + '''''')
+	f.write('''\nversionCode=''' + time.strftime("%j", time.gmtime()) + '''''')
+	f.write('''\nauthor=AdroitAdorKhan''')
+	f.write('''\ndescription=Energized Go!''')
 	f.write('''\nminMagisk=1500''')
 	print('[+] Done!')
